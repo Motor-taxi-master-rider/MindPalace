d46fc0d54bea1bd6189090d40501139322db7dd8
摩的大飚客 <hzy1324@qq.com>
2018-07-16 17:33:14 +0800

    :bug: could not save the same theme when updating documet

--- app/task/forms.py
+++ app/task/forms.py
@@ -1,12 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, SelectField, StringField, SubmitField
+from mongoengine import Q
+from wtforms import IntegerField, SelectField, StringField, SubmitField, HiddenField
 from wtforms.fields.html5 import URLField
 from wtforms.validators import URL, InputRequired, Length, NumberRange, ValidationError
 
 from app.models import Category, DocumentMeta
+from utils import INVALID_OBJECT_ID
 
 
 class DocMetaForm(FlaskForm):
+    id = HiddenField('Id')
     theme = StringField('Theme', validators=[InputRequired(), Length(1, 256)])
     category = SelectField(
         'Category',
@@ -21,5 +24,5 @@ class DocMetaForm(FlaskForm):
     submit = SubmitField('Submit Document')
 
     def validate_theme(self, theme):
-        if DocumentMeta.objects(theme=theme.data).first():
+        if DocumentMeta.objects(Q(theme=theme.data) & Q(id__ne=self.id.data or INVALID_OBJECT_ID)).first():
             raise ValidationError('Theme already exists.')
