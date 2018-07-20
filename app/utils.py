7ca9812344f299f3fc0b6485e4095ab3e9f2fc40
摩的大飚客 <hzy1324@qq.com>
2018-07-20 10:06:49 +0800

    :memo: add comments to app utils

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,6 @@
 from urllib.parse import urljoin, urlparse
 
-from flask import redirect, request, url_for
+from flask import Response, redirect, request, url_for
 
 INVALID_OBJECT_ID = '1' * 24
 
@@ -24,14 +24,16 @@ def index_for_role(role):
     return url_for(role.index)
 
 
-def is_safe_url(target):
+def is_safe_url(target: str) -> bool:
+    """Verdict whether a url is safe to redirect to."""
     ref_url = urlparse(request.host_url)
     test_url = urlparse(urljoin(request.host_url, target))
     return test_url.scheme in ('http', 'https') and \
            ref_url.netloc == test_url.netloc
 
 
-def redirect_back(endpoint, **values):
+def redirect_back(endpoint: str, **values) -> Response:
+    """Redirect to next url if possible, else redirect to endpoint."""
     target = request.args.get('next')
     if not target or not is_safe_url(target):
         target = url_for(endpoint, **values)
