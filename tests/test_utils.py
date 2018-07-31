5cb924483256ba3b81bf96f7f2691c1497864404
摩的大飚客 <gflame42@gmail.com>
2018-07-31 21:56:55 +0800

    :whilte_check_mark: add tests for utils

---
+++ tests/test_utils.py
@@ -0,0 +1,20 @@
+import pytest
+
+from app.models import Category, DocumentMeta
+from app.utils import generate_documents_for_user, get_queue
+
+
+@pytest.mark.usefixtures('db')
+def test_generate_documents_for_user(admin):
+    doc_list = generate_documents_for_user(admin)
+    assert set(doc_list) == set(DocumentMeta.objects(create_by=admin).all())
+    assert len(doc_list) == sum(range(1, len(Category) + 1))
+
+
+@pytest.mark.xfail
+def test_get_queue():
+    queue_boy = get_queue('boy')
+    queuer_same_boy = get_queue('boy')
+    queue_girl = get_queue('girl')
+    assert queue_boy is queuer_same_boy
+    assert queue_boy is not queue_girl
